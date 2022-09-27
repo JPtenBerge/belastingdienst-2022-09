5df1f39d055910e1fc2a34b5c5b047b11ac266a5
JP <jp@jp.nl>
2022-09-27 16:08:20 +0200

    Fakes werkend gekregen!

--- Demo/DemoProject.Unittests/HuidigeClassTest.cs
+++ Demo/DemoProject.Unittests/HuidigeClassTest.cs
@@ -1,26 +1,23 @@
 using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+//using System.fakes;
 
 namespace DemoProject.Unittests;
 
 [TestClass]
 public class HuidigeClassTest
 {
-    [TestMethod]
-    public void Get()
-    {
-        var sut = new HuidigeClass();
-        var result = sut.Get();
+	[TestMethod]
+	public void Get()
+	{
+		var sut = new HuidigeClass();
 
-        using (ShimsContext.Create())
+		using (ShimsContext.Create())
 		{
-            
-
-        }
-
-            Assert.AreEqual("15:10:49", result);
-
-        
-    }
+			System.Fakes.ShimDateTime.NowGet = () => new DateTime(2022, 9, 27, 15, 10, 49);
+			var result = sut.Get();
+			Assert.AreEqual("15:10:49", result);
+		}
+	}
 }
